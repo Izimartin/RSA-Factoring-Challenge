cc617e0b9f891389fde986cde91ea053931948ea
Izimartin <izimartinz@gmail.com>
2022-07-24 03:39:37 +0100

    RSA

--- main.c
+++ main.c
@@ -1,33 +1,18 @@
-#include "factors.h"
+#include "rsa.h"
+
 /**
-  * main - factorise number into a product of two smaller numbers.
-  * @argc: argument count.
-  * @argv: argument vector.
-  *
-  * Return: 0.
-  */
+*main - Entry point
+*@argc: Number of arguments
+*@argv: Array of arguments
+*Return: 0 (success)
+*/
 int main(int argc, char **argv)
 {
-	FILE *fp;
-	size_t size = 0;
-	char *line = NULL, **temp = NULL;
-	unsigned long long int n, p, q;
-
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: factors <file>\n");
+		fprintf(stderr, "USAGE factors file\n");
 		exit(EXIT_FAILURE);
 	}
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
-	{
-		fprintf(stderr, "Could not open file");
-		exit(EXIT_FAILURE);
-	}
-	while (getline(&line, &size, fp) != -1)
-	{
-		n = strtoul(line, temp, 10);
-		printf("%llu\n", n);		
-	}
+	file_op(argv[1]);
 	return (0);
 }
